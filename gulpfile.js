9f00b645739706da5f679785caf79b882090d8f9
igorsilva737 <igor082011@gmail.com>
2020-07-19 20:20:56 -0300

    gulp curso, utilizando dependencias como htmlminify, sassminify e broser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,41 @@ var gulp = require('gulp');
 var html = require('gulp-htmlmin');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
+var notify = require('gulp-notify');
+var browserSync = require('browser-sync').create();
+
 
 //sass
 gulp.task('sass',function(){
     return gulp.src('./src/scss/style.scss')
     .pipe(sass({outputStyle:"compressed"}))
-    .pipe(gulp.dest('./dist/css/'))
+    .on("error", notify.onError("Error: <%= error.message %>"))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream())
 });
 
 // html
 gulp.task('html',function(){
-    return gulp.src('./src/index.html')
+    return gulp.src('./src/template/index.html')
     .pipe(html({collapseWhitespace:true}))
-    .pipe(gulp.dest('./dist'));
+    .on("error", notify.onError("Error: <%= error.message %>"))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 });
+
+
+//broser-sync
+gulp.task('BS',['html','sass'], function() {
+    browserSync.init({
+        server:{
+            baseDir: './dist'
+        }
+    })
+    gulp.watch('./src/index.html',['html'])
+    gulp.watch('./src/scss/style.scss',['sass'])
+});
+
+
+gulp.task('default',['BS']);
+
 
